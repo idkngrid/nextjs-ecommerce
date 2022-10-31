179939f43383019dc542c75428d79adbfd3c9b2a
idkngrid <ingrid.camargopr@gmail.com>
2022-10-31 15:47:27 -0300

    Create and style components

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,37 @@
 import React from 'react'
 
-const Home = () => {
-  return (
-    <>
-      HeroBanner
+import { client } from '../lib/client';
 
-      <div>
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const Home = ({ products, bannerData }) => {
+  return (
+    <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
-      <div>
-        {['Product1', 'Product1'].map((product) => product)} 
+      <div className='products-container'>
+        {products?.map((product) => <Product key={product._id} product={product} />)} 
       </div>
 
-      Footer
-    </>
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home
